8e6626fde18f7d196025ce43d3daf96b172c80c7
Mark Sahlgreen <mark@nozzy.org>
2021-03-24 09:58:40 +0100

    Nav fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,19 +4,19 @@ function Nav() {
   return (
     <nav className="nav-bar">
       <h1>
-        <Link id="logo" to="./">
+        <Link id="logo" to="/">
           Mark Sahlgreen
         </Link>
       </h1>
       <ul>
         <li>
-          <Link to="./">Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="./projects">The Projects</Link>
+          <Link to="/projects">The Projects</Link>
         </li>
         <li>
-          <Link to="./connect">Connect</Link>
+          <Link to="/connect">Connect</Link>
         </li>
       </ul>
     </nav>
